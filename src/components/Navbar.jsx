0021d8eec3fee8631db51676ecb8efbbed8c2365
Junior-Osborn <juniorkhatib7@gmail.com>
2025-08-16 15:30:10 +0300

    NavBar and Hero

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,10 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
+import { useState } from "react";
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const [token, setToken] = useState(true);
+  const navigate = useNavigate();
   return (
     <div className="flex items-center justify-between py-4 mb-5 text-sm border-b border-b-gray-400">
       <img className="w-44 cursor-pointer" src={assets.logo} />
@@ -22,8 +26,42 @@ const Navbar = () => {
           <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
       </ul>
-      <div>
-        <button>Create account</button>
+      <div className="flex items-center gap-4">
+        {token ? (
+          <div className="flex items-center gap-2 cursor-pointer group relative">
+            <img src={assets.profile_pic} className="w-8 rounded-full" />
+            <img src={assets.dropdown_icon} className="w-2.5" alt="" />
+            <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block">
+              <div className="min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4">
+                <p
+                  className="hover:text-black cursor-pointer"
+                  onClick={() => navigate("/my-profile")}
+                >
+                  My Profile{" "}
+                </p>
+                <p
+                  className="hover:text-black cursor-pointer"
+                  onClick={() => navigate("/my-appointment")}
+                >
+                  My Appointment
+                </p>
+                <p
+                  className="hover:text-black cursor-pointer"
+                  onClick={() => setToken(false)}
+                >
+                  Logout
+                </p>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <button
+            onClick={() => navigate("/login")}
+            className="bg-primary text-white py-3 px-8 font-light rounded-full hidden md:block cursor-pointer"
+          >
+            Create account
+          </button>
+        )}
       </div>
     </div>
   );
