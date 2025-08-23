5bd01e503bf00aabb6c3c8098f701e8810afc8ed
Junior-Osborn <juniorkhatib7@gmail.com>
2025-08-23 10:06:34 +0300

    About & Contact page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
-
+import React from "react";
+import { assets } from "../assets/assets";
 const About = () => {
+  const whyUs = [
+    {
+      point: "Efficiency:",
+      explaination:
+        "Streamlined appointment scheduling that fits into your busy lifestyle.",
+    },
+    {
+      point: "Convenience:",
+      explaination:
+        "Access to a network of trusted healthcare professionals in your area.",
+    },
+    {
+      point: "Personalization:",
+      explaination:
+        "Tailored recommendations and reminders to help you stay on top of your health.",
+    },
+  ];
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <div className="text-center text-2xl text-gray-500 pt-10">
+        <h1>
+          ABOUT <span className="text-gray-700 font-medium">US</span>
+        </h1>
+      </div>
+      <div className="flex flex-col md:flex-row gap-12 my-10">
+        <img
+          className="w-full md:max-w-[360px]"
+          src={assets.about_image}
+          alt=""
+        />
+        <div className="flex flex-col gap-6 justify-center md:w-2/4 pt-2 text-sm text-gray-600 ">
+          <p className="">
+            Welcome to Prescripto, your trusted partner in managing your
+            healthcare needs conveniently and efficiently. At Prescripto, we
+            understand the challenges individuals face when it comes to
+            scheduling doctor appointments and managing their health records
+          </p>
+          <p>
+            Prescripto is committed to excellence in healthcare technology. We
+            continuously strive to enhance our platform, integrating the latest
+            advancements to improve user experience and deliver superior
+            service. Whether you're booking your first appointment or managing
+            ongoing care, Prescripto is here to support you every step of the
+            way.
+          </p>
+          <b className="text-gray-800">Our vision</b>
+          <p>
+            Our vision at Prescripto is to create a seamless healthcare
+            experience for every user. We aim to bridge the gap between patients
+            and healthcare providers, making it easier for you to access the
+            care you need, when you need it.
+          </p>
+        </div>
+      </div>
+      <div className="text-xl my-4">
+        <h1>
+          WHY
+          <span className="">CHOOSE US</span>
+        </h1>
+      </div>
+      <div className="flex flex-col md:flex-row mb -20">
+        {whyUs.map((item, index) => (
+          <div className="flex flex-col gap-5 text-[15px] border px-10 md:px-16 py-8 sm:py-16 hover:bg-primary hover:text-white transition-all duration-300">
+            <b>{item.point}</b>
+            <p>{item.explaination}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,40 @@
 import React from "react";
-
+import { assets } from "../assets/assets";
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <div className="text-center text-2xl pt-10 text-gray-500">
+        <h1>
+          CONTACT <span>Us</span>
+        </h1>
+      </div>
+      <div className="flex flex-col md:flex-row gap-10 justify-center my-10 mb-28 text-sm">
+        <img
+          className="w-full md:max-w-[360px]"
+          src={assets.contact_image}
+          alt=""
+        />
+        <div className="flex flex-col justify-center gap-6 items-start">
+          <h1 className="font-semibold text-lg text-gray-600">Our OFFICE</h1>
+          <p className="text-gray-500">
+            54709 Willms Station <br />
+            Suite 350, Washington, USA
+          </p>
+          <p className="text-gray-500">
+            Tel: (415) 555‑0132 <br />
+            Email: greatstackdev@gmail.com
+          </p>
+          <h1 className="font-semibold text-lg text-gray-600">
+            Careers at PRESCRIPTO
+          </h1>
+          <p className="text-gray-500">
+            Learn more about our teams and job openings.
+          </p>
+          <button className="border px-8 py-4 text-sm cursor-pointer hover:bg-black hover:text-white transition-all duration-500">Explore Jobs</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
